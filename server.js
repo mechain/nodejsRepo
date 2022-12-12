06b136dd62010d712e71d0abe157bd9314f370da
Shikhar <shikhar151999@gmail.com>
2022-12-12 15:54:31 +0530

    template engine html rendering

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json())
+
 
 const task = require("./route");
+
+
 //creating the route '/'
-
-
-
+app.set('view engine' , 'pug');
 app.get('/' , (req, res ) => {
     res.status(200).send("Welcome To Nodejs");
 });
